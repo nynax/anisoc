c6fc00b68482d1e572673184491dc8d778150a47
Ivan Babayan <nynax@mail.ru>
2022-09-27 19:28:18 +0300

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import Navbar from "./components/Navbar/Navbar"
+import {Profile} from "./components/Pages/Profile/Profile"
+import News from "./components/Pages/News/News"
+import Music from "./components/Pages/Music/Music"
+import Settings from "./components/Pages/Settings/Settings"
+import Footer from "./components/Footer/Footer"
+import {Route, Routes, BrowserRouter} from "react-router-dom"
+import DialogsContainer from "./components/Pages/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Pages/Users/UsersContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = (props) => {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <div className='header'>
+                    <HeaderContainer store={props.store}/>
+                </div>
+                <div className='navbar'>
+                    <Navbar store={props.store}/>
+                </div>
+                <div className='content'>
+                    <Routes>
+                        <Route path='/dialogs/*' element={<DialogsContainer store={props.store}/>}/>
+                        <Route path='/users' element={<UsersContainer store={props.store}/>}/>
+
+                        <Route path="/profile/" element={<Profile store={props.store}/>}/>
+                        <Route path="/profile/:userId" element={<Profile store={props.store}/>}/>
+
+
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
+                        <Route path='/*' element={<Profile store={props.store}/>}/>
+                        <Route path='/' element={<Profile store={props.store}/>}/>
+                    </Routes>
+                </div>
+                <div className='footer'>
+                    <Footer/>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css"
+import reportWebVitals from "./reportWebVitals";
+import store  from "./redux/reduxStore";
+import ReactDOM from "react-dom/client";
+import React from "react";
+import App from "./App";
+import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//hakam69745@dnitem.com
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    //<React.StrictMode>
+        <Provider store={store}>
+            <App store={store}/>
+        </Provider>
+    //</React.StrictMode>
 );
+
+window.store = store
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
